36d6411f5ff160b5a960229ad46bb3f1ecd50ab4
zx108547 <32323949+zx108547@users.noreply.github.com>
2018-03-26 19:56:51 +0800

    menu bar
    data import and export to .json file

---
+++ References/Xiang.Zhang/my_test/src/renderer/components/Editor.vue
@@ -0,0 +1,247 @@
+<template>
+  <div>
+    <div class="container">
+      <div class='input' v-for="item in mycars">{{item}}<!--@input="onEditorCodeChange"-->
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+const fs = require('fs')
+const math = require('mathjs')
+const Menu = require('../modules/menu.js')
+var matrix = math.matrix([[7, 1], [-2, 3]])
+
+export default {
+  name: 'mii-editor',
+  data () {
+    return {
+      mycars: ['2', '3', '4'],
+      code: '',
+      isPreview: true,
+      input: '',
+      path: '',
+    }
+  },
+  mounted () {
+    Menu.togglePreview = this.togglePreview
+    Menu.openFile = this.openFile
+    Menu.saveFile = this.saveFile
+    Menu.saveAsFile = this.saveAs
+    Menu.ready()
+    this.openLinkExternal()
+  },
+
+  methods: {
+    openDialog (type, msg) {
+      const remote = this.$electron.remote
+      const dialog = remote.dialog
+      const browserWindow = remote.BrowserWindow
+      const focusedWindow = browserWindow.getFocusedWindow()
+
+      dialog.showMessageBox(focusedWindow, {
+        title: type,
+        type: type,
+        buttons: ['OK'],
+        detail: msg
+      })
+    },
+    newFile () {
+      const editor = this.$refs.codemirror.editor
+
+      // is newFile
+      if (!this.path && editor.isClean()) {
+        return
+      }
+
+      // is not modify
+      if (this.path && !editor.isClean()) {
+        this.clean()
+        return
+      }
+
+      let response = this.modifyDialog()
+      switch (response) {
+        // Yes
+        case 0:
+          this.saveFile()
+          break
+        // No
+        case 1:
+          this.clean()
+          break
+      }
+    },
+    openFile () {
+      const self = this
+      const remote = this.$electron.remote
+      const dialog = remote.dialog
+      const browserWindow = remote.BrowserWindow
+      const focusedWindow = browserWindow.getFocusedWindow()
+
+      dialog.showOpenDialog(focusedWindow, {
+        title: 'Open Dialog',
+        filters: [{
+          name: 'Documents',
+          extensions: ['txt', 'json']
+        }],
+        properties: ['openFile']
+      }, function (item) {
+        if (item) {
+          self.readFile(item[0])
+        }
+      })
+    },
+    readFile (path) {
+      const self = this
+
+      fs.readFile(path, 'utf8', function (err, content) {
+        if (err === null) {
+          var data = JSON.parse(content)
+          matrix = data
+          self.setPath(path)
+        } else {
+          self.openDialog('error', err.toString())
+        }
+      })
+    },
+    saveAsDialog () {
+      const remote = this.$electron.remote
+      const dialog = remote.dialog
+      const browserWindow = remote.BrowserWindow
+      const focusedWindow = browserWindow.getFocusedWindow()
+
+      let savePath = dialog.showSaveDialog(focusedWindow, {
+        title: 'Save Dialog',
+        filters: [
+          {name: 'Text file', extensions: ['json']}
+        ]
+      })
+
+      return savePath
+    },
+    modifyDialog () {
+      const remote = this.$electron.remote
+      const dialog = remote.dialog
+      const browserWindow = remote.BrowserWindow
+      const focusedWindow = browserWindow.getFocusedWindow()
+
+      let response = dialog.showMessageBox(focusedWindow, {
+        title: '',
+        type: 'warning',
+        buttons: ['Yes', 'No', 'Cancel'],
+        detail: 'Wolud you like to save changes?'
+      })
+
+      return response
+    },
+    saveFile () {
+      let result
+
+      if (this.path) {
+        this.setPath(this.path)
+        result = this.writeFile()
+      } else {
+        let savePath = this.saveAsDialog()
+        if (savePath) {
+          this.setPath(savePath)
+          result = this.writeFile()
+        }
+      }
+    },
+    saveAs () {
+      const self = this
+      let savePath = this.saveAsDialog()
+
+      if (savePath) {
+        self.setPath(savePath)
+        let result = self.writeFile()
+      }
+    },
+    writeFile () {
+      const self = this
+      try {
+        let error
+
+        fs.writeFile(self.path, JSON.stringify(matrix), function (err) {
+          error = err
+        })
+
+        if (!error) {
+          return true
+        }
+      } catch (e) {
+        self.openDialog('error', e)
+        return false
+      }
+
+      return false
+    },
+    setPath (path) {
+      this.path = path
+    },
+    setEditor (value) {
+      this.$refs.codemirror.editor.setValue(value)
+      this.$refs.codemirror.editor.save()
+    },
+    clean () {
+      this.setEditor('')
+      this.setPath('')
+      this.$refs.codemirror.editor.markClean()
+      this.$refs.codemirror.editor.clearHistory()
+    },
+    dropFile (file, ext) {
+      const self = this
+      const remote = this.$electron.remote
+      const dialog = remote.dialog
+      const browserWindow = remote.BrowserWindow
+      const focusedWindow = browserWindow.getFocusedWindow()
+
+      if (file.type === 'text/plain' || file.type === 'application/text' || ext === 'txt' || ext === 'md') {
+        self.readFile(file.path)
+      } else {
+        dialog.showMessageBox(focusedWindow, {
+          title: 'error',
+          type: 'error',
+          buttons: ['OK'],
+          detail: 'This file format is not supported.'
+        })
+      }
+    },
+    openLinkExternal () {
+      const electron = this.$electron
+      const window = electron.remote.getCurrentWindow()
+
+      document.addEventListener('click', function (e) {
+        let target = e.target
+        let href = target.getAttribute('href')
+
+        if (target.tagName !== 'A' && !href) {
+          return
+        }
+
+        if (href.substring(0, 4) === 'http') {
+          e.preventDefault()
+          // get status
+          let status = window.isAlwaysOnTop()
+          // on top
+          window.setAlwaysOnTop(true)
+          // open link
+          electron.shell.openExternal(target.href)
+          // restore
+          if (!status) {
+            setTimeout(function () {
+              window.setAlwaysOnTop(false)
+            }, 1000)
+          }
+        }
+      })
+    }
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ References/Xiang.Zhang/my_test/src/renderer/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <editor ref='mii-editor'></editor>
+  </div>
+</template>
+
+<script>
+  import Editor from '@/components/Editor'
+
+  export default {
+    name: 'mii-main',
+    methods: {},
+    mounted: function () {},
+    components: {
+      Editor,
+    }
+  }
+</script>
+
+<style scoped></style>
