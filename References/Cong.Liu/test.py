055517ae866dbd51a4fab782da1af00229406081
Alan-wq <28079607@qq.com>
2018-03-26 08:59:58 +0800

    Update test.py

--- References/Cong.Liu/test.py
+++ References/Cong.Liu/test.py
@@ -1,6 +1,8 @@
 #! usr/bin/env python3
 import numpy as np
-import scipy.linalg as sl
+import matplotlib.pyplot as plt
+
+
 """
 p0 = np.matrix('1,1,1; 2,3,4')
 n = np.size(p0,1)
@@ -35,19 +37,6 @@ def feval(funt,var):
 '''
 
 
-def resampling(q):
-    qc = cumsum(q)
-    M = np.max(q.shape)
-    u = (np.arange(0, M) + np.random.uniform(0, 1, 1))/M
-    i = np.zeros((1, M))
-    k = 0
-    for j in range(0,M):
-        while (qc[0][k] < u[0][j]) :
-            k = k + 1
-        i[0][j] = k
-    return i
-
-
 def PF(f,h,pe,Q,P0,M,y):
 
     n = np.size(P0,1)
@@ -56,12 +45,26 @@ def PF(f,h,pe,Q,P0,M,y):
     for t in range(0,100):
         e = np.kron(np.ones((1,M)), y[0][t]) - h(x)
         q = pe(e)
-        q = q/np.sum(q)
+        q = np.divide(q,np.sum(q))
         xhat[0][t] = np.sum(np.multiply(np.kron(np.ones((n,1)),q), x), 1)
         ind = resampling(q)
-        x = x[:,ind]
+        ind = ind.astype(int)
+        x = x[0][ind]
         x = f(x,t) + np.sqrt(Q)*np.random.randn(n,M)
     return xhat
+
+
+def resampling(q):
+    qc = cumsum(q)
+    M = np.max(q.shape)
+    u = (np.arange(0, M) + np.random.uniform(0, 1, 1))/M
+    i = np.zeros((1, M))
+    k = 0
+    for j in range(0,M):
+        while qc[k] < u[j]:
+            k = k + 1
+        i[0][j] = k
+    return i
 
 
 def TestPF():
@@ -70,23 +73,26 @@ def TestPF():
     Q = 10
     R = 1
     pe = lambda x : ((1/((2*np.pi*R)**(1/2)))*np.exp(-(x**2)/(2*R)))
-    f = lambda x,t : ((x**2) + (25*x)/(1+x**2) + 8*np.cos(1.2*t))
+    f = lambda x, t : ((x/2) + (25*x)/(1+x**2) + 8*np.cos(1.2*t))
     h = lambda x : ((x**2)/20)
-    x = np.arange(1,101)
-    y = np.arange(1,101)
-    x[0] = sl.sqrtm(P0)*np.random.randn(1)
+    x = np.array([np.arange(1,101)])
+    y = np.array([np.arange(1,101)])
+    x[0] = np.sqrt(P0)*np.random.randn(1)
     y[0] = h(x[0]) + np.sqrt(R)*np.random.randn(1)
 
-    for t in range(2,101):
-        x[t-1] = f(x[t-1], t)
-        y[t-1] = h(x[t-1]) + np.sqrt(R)*np.random.randn(1)
+    for t in range(1,100):
+        x[0][t] = f(x[0][t-1], t-1)
+        y[0][t] = h(x[0][t]) + np.sqrt(R)*np.random.randn(1)
 
     xTrue = x
     xhat = PF(f,h,pe,Q,P0,M,y)
-    return xTrue
-
+    plt_x = [i for i in range(1,101)]
+    plt.plot(plt_x, xhat[0], '--', plt_x, xTrue[0],'r')
+    plt.show(block=True)
+    #return xTrue
 
 TestPF()
+#print(TestPF())
 '''
 b = np.array([[1,16],[256,81]])
 print(b)
