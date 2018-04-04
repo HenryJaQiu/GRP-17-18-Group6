c78805470f7334c0b7f814e98fd556a7302c2ecc
Henry <605672453@qq.com>
2018-04-04 18:10:24 +0800

    Chart Error Fix

--- Particle Filter Simulator/src/renderer/components/LandingPage/FilterChart.vue
+++ Particle Filter Simulator/src/renderer/components/LandingPage/FilterChart.vue
@@ -9,7 +9,7 @@
 <script>
   import {template} from '../../menu.js'
   const {remote} = require('electron')
-  // const math = require('mathjs')
+  const math = require('mathjs')
   var matrixSpace = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100']
   const {Menu} = remote
   const menu = Menu.buildFromTemplate(template)
@@ -100,15 +100,23 @@
         this.myChart = this.echarts.init(document.getElementById('myChart'))
       },
       loadMatrix () {
-        console.log(this.$store.getters.getMatrixXhat)
-        console.log(this.$store.getters.getMatrixTrue)
-        this.option1.series[0].data = this.$store.getters.getMatrixXhat
-        this.option1.series[1].data = this.$store.getters.getMatrixTrue
+        console.log(this.convertDTO(this.$store.getters.getMatrixXhat))
+        console.log(this.convertDTO(this.$store.getters.getMatrixTrue))
+        this.option1.series[0].data = this.convertDTO(this.$store.getters.getMatrixXhat)
+        this.option1.series[1].data = this.convertDTO(this.$store.getters.getMatrixTrue)
       },
       drawChart () {
         console.log('tran3')
         this.loadMatrix()
         this.myChart.setOption(this.option1)
+      },
+      convertDTO (mat) {
+        var matSize = math.subset(math.size(mat), math.index(1))
+        var matDTO = Array(matSize)
+        for (let i = 0; i < matSize; i++) {
+          matDTO[i] = mat.subset(math.index(0, i))
+        }
+        return matDTO
       }
     }
   }
