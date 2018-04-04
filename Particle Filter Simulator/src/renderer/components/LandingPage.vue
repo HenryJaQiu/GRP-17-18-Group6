6b5cba28f16c4aecb059d87960f88f810074a221
Alan-wq <28079607@qq.com>
2018-04-04 13:54:31 +0800

    Update LandingPage.vue

--- Particle Filter Simulator/src/renderer/components/LandingPage.vue
+++ Particle Filter Simulator/src/renderer/components/LandingPage.vue
@@ -18,9 +18,11 @@
 <script>
   import FilterChart from './LandingPage/FilterChart'
   import ParameterController from './LandingPage/ParameterController'
-
+  import store from '../vuex/store'
+  
   export default {
     name: 'landing-page',
+    store,
     components: { FilterChart, ParameterController },
     methods: {
     }
